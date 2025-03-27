dd3d5fea1235cca33f1cebb6557ecb74cba7af92
Ricardo Marques <dataricardom@gmail.com>
2025-03-27 17:38:38 -0300

    Comparando com SQL e ordenando colunas com pandas

--- dia_02/05_colunas.py
+++ dia_02/05_colunas.py
@@ -33,22 +33,77 @@ df.dtypes
 
 #O parametro columns utiliza um dicionario onde a chave é o nome da coluna a ser modificada e o valor o novo nome.
 rename_column = {
-    "dtCriacao": "Criacao"
+    "descSistemaOrigem": "SistemaOrigem",
 }
 
 # Maneira usando variavel.
 
-df.columns(columns = rename_column)
+df.rename(columns = rename_column)
 
+# %%
 df_1 = df.rename(columns={"qtdePontos":"qtPontos"})
 
+# %%
 #Altera o proprio dataframe sem precisar criar outro
 df.rename(columns={"qtdePontos":"qtPontos"}, inplace=True)
 
 
 # %%
+df.rename(columns={"qtdePontos":"qtPontos"}, inplace=True)
 
 df_1.head()
 
 
 # %%
+
+#Retorna uma serie.
+
+df["idCliente"]
+
+#Retorna um Dataframe.
+
+df[["idCliente"]]
+
+
+# %%
+#COMPARANDO FUNÇÕES NO SQL NO PANDAS
+
+
+# SELECT * FROM df
+
+df
+
+# %%
+
+#SELECT qtPontos FROM df
+
+df[["qtPontos"]]
+
+# %%
+
+#SELECT * FROM df limit 10
+
+df.head(n=10)
+
+# %%
+
+#SELECT idCliente, qtPontos FROM df LIMIT 10
+
+df[["idCliente", "qtPontos"]].head(n=10)
+
+# %%
+
+# Reordenando ordem das colunas: Só colocar na lista de colunas a ordem que deseja.
+
+#SELECT idCliente, idTransacao, qtPontos FROM  df LIMIT 10
+
+df[["idCliente", "idTransacao", "qtPontos"]].head(10)
+
+# %% 
+# Ordenando em ordem alfabetica
+
+colunas = list(df.columns)
+
+colunas.sort()
+
+colunas
