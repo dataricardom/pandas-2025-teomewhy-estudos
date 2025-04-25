514d1976d689bb8b956b63414f889093ed7a4abc
Ricardo Marques <dataricardom@gmail.com>
2025-04-25 15:54:37 -0300

    Melhorando estrutura do codigo

--- dia_09/query.sql
+++ dia_09/query.sql
@@ -1,6 +1,7 @@
-select o.ship_city as cidade, sum(od.unit_price)as total_cidade
-from orders as o
-left join order_details as od
-on o.order_id = od.order_id
-group by cidade
-order by total_cidade desc;
+SELECT o.ship_city AS cidade,
+       SUM(od.unit_price) AS total_cidade
+FROM orders AS o
+LEFT JOIN order_details AS od
+       ON o.order_id = od.order_id
+GROUP BY cidade
+ORDER BY total_cidade DESC;
